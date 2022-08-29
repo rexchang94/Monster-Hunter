e139c000c344d0b3fe26e8d7555c77ddd95c4e55
rex <rexchang94@gmail.com>
2022-08-29 13:43:58 +0800

    --Move 1. move model/ElementRepository to repository/ElementRepository

--- monster-hunter/src/main/java/com/capcom/mh/model/ElementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capcom.mh.model;
-
-import com.capcom.mh.entity.Element;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ElementRepository extends JpaRepository<Element, Long> {
-}
